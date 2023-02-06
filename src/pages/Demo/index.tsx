4da27464887a70b6a1bdfbcfbc958496bd4c2f4e
xunchang528 <101688822+xunchang528@users.noreply.github.com>
2023-02-06 14:13:25 +0800

    建立服务器端请求数据体系

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -60,4 +60,8 @@ const mapDispatchToProps = (dispatch: any) => {
 
 const storeDemo: any = connect(mapStateToProps, mapDispatchToProps)(Demo);
 
+storeDemo.getInitProps = (store: any, data?: string) => {
+  return store.dispatch(getDemoData(data || "这是初始化的demo"));
+};
+
 export default storeDemo;
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,25 +1,27 @@
 import express from "express";
 import childProcess from "child_process";
-import { renderToString } from "react-dom/server";
 import path from "path";
-import router from "@/router";
 import { Route, Routes } from "react-router-dom";
+import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
-import { Helmet } from "react-helmet";
+import { matchRoutes, RouteObject } from "react-router-dom";
+import router from "@/router";
 import { serverStore } from "@/store";
 import { Provider } from "react-redux";
+import { Helmet } from "react-helmet";
 
 const app = express();
 
 const bodyParser = require("body-parser");
 
-app.use(express.static(path.resolve(process.cwd(), "client_build")));
-
 // 请求body解析
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// 启一个post服务
+// 注入事件处理的脚本
+app.use(express.static(path.resolve(process.cwd(), "client_build")));
+
+// demo api
 app.post("/api/getDemoData", (req, res) => {
   res.send({
     data: req.body,
@@ -28,21 +30,41 @@ app.post("/api/getDemoData", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  const content = renderToString(
-    <Provider store={serverStore}>
-      <StaticRouter location={req.path}>
-        <Routes>
-          {router?.map((item, index) => {
-            return <Route {...item} key={index} />;
-          })}
-        </Routes>
-      </StaticRouter>
-    </Provider>
-  );
+  const routeMap = new Map<string, () => Promise<any>>(); // path - loaddata 的map
+  router.forEach((item) => {
+    if (item.path && item.loadData) {
+      routeMap.set(item.path, item.loadData(serverStore));
+    }
+  });
 
-  const helmet = Helmet.renderStatic();
+  // 匹配当前路由的routes
+  const matchedRoutes = matchRoutes(router as RouteObject[], req.path);
 
-  res.send(`
+  const promises: Array<() => Promise<any>> = [];
+  matchedRoutes?.forEach((item) => {
+    if (routeMap.has(item.pathname)) {
+      promises.push(routeMap.get(item.pathname) as () => Promise<any>);
+    }
+  });
+
+  Promise.all(promises).then((data) => {
+    // 统一放到state里
+    // 编译需要渲染的JSX, 转成对应的HTML STRING
+    const content = renderToString(
+      <Provider store={serverStore}>
+        <StaticRouter location={req.path}>
+          <Routes>
+            {router?.map((item, index) => {
+              return <Route {...item} key={index} />;
+            })}
+          </Routes>
+        </StaticRouter>
+      </Provider>
+    );
+
+    const helmet = Helmet.renderStatic();
+
+    res.send(`
     <html
       <head>
         ${helmet.title.toString()}
@@ -50,10 +72,16 @@ app.get("*", (req, res) => {
       </head>
       <body>
         <div id="root">${content}</div>
+        <script>
+          window.context = {
+            state: ${JSON.stringify(serverStore.getState())}
+          }
+        </script>
         <script src="/index.js"></script>
       </body>
     </html>
   `);
+  });
 });
 
 app.listen(3000, () => {
